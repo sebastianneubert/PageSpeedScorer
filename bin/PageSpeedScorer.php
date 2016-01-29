17b9a93f8b6fbb3c8ac558d0c1cb30f29f3e0baf
Nils Langner <contact@phmlabs.com>
2016-01-29 22:07:00 +0100

    initial draft

---
+++ bin/PageSpeedScorer.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once __DIR__ . "/../vendor/autoload.php";
+
+if (count($argv) < 5) {
+    echo "\n    PageSpeedScorer - Version ##development##";
+    die("\n\n    Usage: PageSpeedScorer.phar url options [system_identifier] [project_api_key] [koalamonserver]\n\n");
+}
+
+$url = $argv[1];
+
+$options = json_decode($argv[2]);
+if (!is_null($options) && $options !== false && property_exists($options, 'limit')) {
+    $limit = $options->limit;
+}
+
+$system = $argv[3];
+$apiKey = $argv[4];
+
+if ($argv[5]) {
+    $reporter = new \Koalamon\Client\Reporter\Reporter('', $apiKey, new \GuzzleHttp\Client(), $argv[5]);
+} else {
+    $reporter = new \Koalamon\Client\Reporter\Reporter('', $apiKey, new \GuzzleHttp\Client());
+}
+
+$pageSpeed = new \PageSpeed\Insights\Service();
+$psResult = $pageSpeed->getResults($url);
+$pageSpeedScore = $psResult["ruleGroups"]["SPEED"]["score"];
+
+$pageSpeedUrl = "https://developers.google.com/speed/pagespeed/insights/?url=" . $url;
+
+if ($pageSpeedScore < $limit) {
+    $status = \Koalamon\Client\Reporter\Event::STATUS_FAILURE;
+    $message = "Page Speed Score low (" . $pageSpeedScore . ").";
+} else {
+    $status = \Koalamon\Client\Reporter\Event::STATUS_SUCCESS;
+    $message = "";
+}
+
+$event = new \Koalamon\Client\Reporter\Event('pagespeed_' . $url, $system, $status, 'pagespeedscore', $message, $pageSpeedScore, $url);
+$reporter->sendEvent($event);
+
+echo "\n    $message\n\n";
